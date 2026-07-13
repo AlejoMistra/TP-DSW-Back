bc7f092a94a728239373715419a1e090d4562ae6
AlejoMistra <mistralettialejo@gmail.com>
2026-07-12 21:59:21 -0300

    feat: add MembershipPlan entity and initial membership plans data

---
+++ src/membershipPlan/plan.entity.ts
@@ -0,0 +1,23 @@
+export interface PlanProperties {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  durationDays: number;
+}
+
+export class MembershipPlan {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  durationDays: number;
+
+  constructor(props: PlanProperties) {
+    this.id = props.id;
+    this.description = props.description;
+    this.name = props.name;
+    this.price = props.price;
+    this.durationDays = props.durationDays;
+  }
+}
