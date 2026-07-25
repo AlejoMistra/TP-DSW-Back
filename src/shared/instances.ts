e437a0a75c5b1f7691de0a4acf97b40fd96c00a8
AlejoMistra <mistralettialejo@gmail.com>
2026-07-24 23:34:49 -0300

    feat: add PrismaClient initialization and update repository imports

--- src/shared/instances.ts
+++ src/shared/instances.ts
@@ -3,9 +3,11 @@ import { MemberRepository } from '../modules/member/member.repository.js';
 import { ExerciseRepository } from '../modules/exercise/exercise.repository.js';
 import { PlanRepository } from '../modules/membershipPlan/plan.repository.js';
 import { ClassScheduleRepository } from '../modules/classSchedule/classSchedule.repository.js';
+import { PrismaClient } from '@prisma/client/extension';
 
 export const instructorRepository = new InstructorRepository();
 export const memberRepository = new MemberRepository();
 export const exerciseRepository = new ExerciseRepository();
 export const planRepository = new PlanRepository();
 export const classScheduleRepository = new ClassScheduleRepository();
+export const prisma = new PrismaClient();
