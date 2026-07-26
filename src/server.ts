e3ad99a67c3a44f1acc03211858e0c751b9d04af
Iael Leonardo Borda <iaelborda04@gmail.com>
2026-07-26 20:19:12 -0300

    chore: revierto cambios en archivos de configuracion de pnpm

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 import { app } from './app.js';
 
-const PORT = Number(process.env.PORT ?? 3000);
+const PORT = process.env.PORT ?? 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
