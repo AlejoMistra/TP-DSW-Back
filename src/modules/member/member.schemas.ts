3463e8080b25cd4edef07757eea541336488aae8
AlejoMistra <mistralettialejo@gmail.com>
2026-08-14 08:58:02 -0300

    refactor: update member schemas to improve structure and consistency

--- src/modules/member/member.schemas.ts
+++ src/modules/member/member.schemas.ts
@@ -1,12 +1,10 @@
-import { z } from 'zod';
+ import { z } from 'zod';
+import { Membership, Status } from '../../generated/prisma/client';
 
-// MemberIdSchema podria borrarse
-// Schema para obtener un socio por ID (parámetro de ruta)
 export const MemberIdSchema = z.object({
   id: z.string().regex(/^\d+$/, 'ID debe ser un número').transform(Number),
 });
 
-// Schema para crear un socio (POST - sin ID ni fechaIngreso)
 export const CreateMemberSchema = z.object({
   name: z
     .string()
@@ -22,22 +20,13 @@ export const CreateMemberSchema = z.object({
     .regex(/^\d{7,15}$/, 'Teléfono debe tener entre 7 y 15 dígitos')
     .nullable()
     .optional(),
+  joinDate: z.date(),
+  //membershipPlan: z.ZodType<Membership>(),
+  status: z.enum(Status),
 });
 
-// Schema para actualizar (solo name, surname, email, phone)
-export const UpdateMemberSchema = z.object({
-  name: z.string().min(2).max(100).optional(),
-  surname: z.string().min(2).max(100).optional(),
-  email: z.email().optional(),
-  phone: z
-    .string()
-    .regex(/^\d{7,15}$/)
-    .nullable()
-    .optional(),
-  status: z.enum(['ACTIVE', 'INACTIVE']).optional(), // Solo en update
-});
+export const UpdateMemberSchema = CreateMemberSchema.partial();
 
-// Schema de respuesta (con ID y fechaIngreso)
 export const MemberResponseSchema = z.object({
   id: z.number(),
   name: z.string(),
@@ -45,14 +34,12 @@ export const MemberResponseSchema = z.object({
   email: z.string(),
   phone: z.string().nullable(),
   joinDate: z.date(),
-  status: z.enum(['ACTIVE', 'INACTIVE']),
+  status: z.enum(Status),
   createdAt: z.date(),
   updatedAt: z.date(),
   deletedAt: z.date().nullable(),
 });
 
-// Revisar esto
-// Tipos inferidos automáticamente de TypeScript
 export type CreateMemberInput = z.infer<typeof CreateMemberSchema>;
 export type UpdateMemberInput = z.infer<typeof UpdateMemberSchema>;
 export type MemberResponse = z.infer<typeof MemberResponseSchema>;
